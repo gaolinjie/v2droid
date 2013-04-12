009159cf6ef0cc27129ba0b9faaa3ad663d6db30
gaolinjie <gaolinjie@gmail.com>
2013-04-12 19:01:17 +0800

    setup the project

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 v2droid
 =======
 
-Some info: [v2droid intro](http://v2ex.com/t/54730)
+###Some info:  
+[v2droid intro](http://v2ex.com/t/54730)  
+   
+   
+###Used libs:  
+[ActionBarSherlock](https://github.com/JakeWharton/ActionBarSherlock)  
+[SlidingMenu](https://github.com/jfeinstein10/SlidingMenu)  
+[Android-ViewPagerIndicator](https://github.com/JakeWharton/Android-ViewPagerIndicator)   
+[Android-PullToRefresh](https://github.com/chrisbanes/Android-PullToRefresh)   
+[Crouton](https://github.com/keyboardsurfer/Crouton)   
+[Crouton](https://github.com/loopj/android-async-http)   
+   
+  
+###How to setup:  
+1. Import the dependency libs at v2droid/libs-src/, and there should be some libs need to rename.  
+2. Import the v2droid project.  
+3. Build the libs and then run the v2droid.  
---
+++ v2droid/src/com/v2ex/v2droid/Splash.java
@@ -0,0 +1,36 @@
+package com.v2ex.v2droid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity {
+	
+	public static final String SHOW_CONTENT = "me.zgeek.v2ex.ContentActivity.SHOW_CONTENT";
+
+	/**
+	 * 延期时间
+	 */
+	private final int SPLASH_DISPLAY_LENGHT = 1000;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		/**
+		 * 使用handler来处理
+		 */
+		new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				Intent mainIntent = new Intent(Splash.this, TopicActivity.class);
+				Splash.this.startActivity(mainIntent);
+				Splash.this.finish();
+			}
+
+		}, SPLASH_DISPLAY_LENGHT);
+	}
+}
